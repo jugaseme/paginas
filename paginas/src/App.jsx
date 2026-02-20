dabb826da39dfcb77be0ee9a3a03c13d83ad6e43
jugaseme <sebastiangarciamedina26@gmail.com>
2026-02-20 09:12:44 -0500

    mejoritas

--- paginas/src/App.jsx
+++ paginas/src/App.jsx
@@ -7,6 +7,31 @@ import Admin from "./pages/Admin"
 import Cart from "./pages/Cart"
 
 function App() {
+
+  const defaultProducts = [
+  {
+    id: 1,
+    name: "Camiseta Negra",
+    price: 80000,
+    category: "Ropa",
+    image: "https://picsum.photos/400?random=1"
+  },
+  {
+    id: 2,
+    name: "Gorra Street",
+    price: 50000,
+    category: "Accesorios",
+    image: "https://picsum.photos/400?random=2"
+  },
+  {
+    id: 3,
+    name: "Chaqueta Oversize",
+    price: 150000,
+    category: "Ropa",
+    image: "https://picsum.photos/400?random=3"
+  }
+]
+
   const [products, setProducts] = useState([])
   const [cart, setCart] = useState([])
 
--- paginas/src/pages/Home.jsx
+++ paginas/src/pages/Home.jsx
@@ -1,10 +1,42 @@
 import "./Home.css"
+import { useState } from "react"
 
-function Home({ products, cart, setCart }) {
+function Home({ cart, setCart }) {
+   const defaultProducts = [
+  {
+    id: 1,
+    name: "Camiseta Negra",
+    price: 80000,
+    category: "Ropa",
+    image: "https://picsum.photos/400?random=1"
+  },
+  {
+    id: 2,
+    name: "Gorra Street",
+    price: 50000,
+    category: "Accesorios",
+    image: "https://picsum.photos/400?random=2"
+  },
+  {
+    id: 3,
+    name: "Chaqueta Oversize",
+    price: 150000,
+    category: "Ropa",
+    image: "https://picsum.photos/400?random=3"
+  }
+]
+  
+const [products, setProducts] = useState(() => {
+  const stored = localStorage.getItem("products")
+  return stored ? JSON.parse(stored) : defaultProducts
+})
 
   const addToCart = (product) => {
     setCart(prev => [...prev, product])
   }
+  const deleteProduct = (id) => {
+  setProducts(products.filter(product => product.id !== id))
+}
 
   return (
     <div className="home-container">
@@ -28,6 +60,9 @@ function Home({ products, cart, setCart }) {
             <button onClick={() => addToCart(product)}>
               Agregar al carrito
             </button>
+           <button onClick={() => deleteProduct(product.id)}>
+  Eliminar
+</button>
           </div>
         ))}
       </div>
